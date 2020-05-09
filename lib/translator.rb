f5c0eda6fcad38b1187747dbd60fafb283ac1442
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-09 17:04:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -30,7 +30,7 @@ def get_english_meaning(file_path,
   japanese_emoticon)
   library = load_library(file_path)
   if library.include?(japanese_emoticon)
-    get_meaning = key
+    get_meaning =
   
   end 
 end
