629667bd34f1f15d6ca16649517c41abb762be78
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-09 17:33:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -32,7 +32,7 @@ def get_japanese_emoticon(file_path,
     library["get_j_emoticon"][english_emoticon] 
     
   else 
-    puts "Sorry, that emoticon was not found."
+    "Sorry, that emoticon was not found."
   end 
 end
 
