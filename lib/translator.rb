6a5bbfa13c4d2074bbdef88300f1aaa229f55fe5
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-09 17:06:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,9 @@ def load_library(file_path)
       new_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, 
+  english_emoticon)
+  library = load_library(file_path)
   
 end
 
